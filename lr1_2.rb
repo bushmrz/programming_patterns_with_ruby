0d059abbf97a13c5052b5354f3cbc8f230b6991b
bushmrz <56377675+bushmrz@users.noreply.github.com>
2023-02-07 23:58:12 +0300

    2_3 done

--- lr1_2.rb
+++ lr1_2.rb
@@ -28,3 +28,34 @@ def method2(num)
 end
 
 method2(5423786)
+
+# 3 
+def gcd(del, x)
+	if (x == 0)
+		return del
+	end 
+	return gcd(x, del % x)
+end	
+
+def method3(num)
+	sum = 0
+	sum_dig = 0
+	mean_dig = 1
+
+	num.digits.each {
+		|dig|
+		sum_dig+=dig
+		mean_dig*=dig
+	}
+
+	num.downto(1).each {
+		|del|
+		if num%del == 0 && gcd(del, sum_dig) == 1 && (gcd(del, mean_dig)) != 1
+		sum+=del 
+		end
+	}
+
+	puts "Sum is #{sum}"
+end
+
+method3(175)
