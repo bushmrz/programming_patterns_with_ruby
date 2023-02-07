e77a1cc90268bafdfba890e9971320a341480cf8
bushmrz <56377675+bushmrz@users.noreply.github.com>
2023-02-07 09:39:44 +0300

    1_1

---
+++ lr1_1.rb
@@ -0,0 +1,17 @@
+puts 'Hello  World'
+
+s = `whoami`
+puts 'Hello #{s}. Ur fav lang?'
+case 'java'
+	when 'c++'
+		puts 'cool'
+	when 'java'
+		puts 'u re crazy'
+	when 'ruby'
+		puts 'podliza'
+	else 
+		puts 'okay'
+	end
+
+
+		
