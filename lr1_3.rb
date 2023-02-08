54f06efc476ed5d09c92cd2912cd7dac94a277c2
bushmrz <56377675+bushmrz@users.noreply.github.com>
2023-02-08 23:22:27 +0400

    done 1_3

--- lr1_3.rb
+++ lr1_3.rb
@@ -16,6 +16,27 @@ def find_first_pos(arr)
 	return puts -1 
 end
 
-arr = [-4, -5, 9, 0, 14, 19, -14, 13, 1, -9, 7, 6]
-find_min(arr)
-find_first_pos(arr)
+#arr = [-4, -5, 9, 0, 14, 19, -14, 13, 1, -9, 7, 6]
+#find_min(arr)
+#find_first_pos(arr)
+
+if ARGV.count < 2 
+	puts "Input method and path to file"
+	return
+end 
+
+meth = ARGV[0]
+path = ARGV[1]
+
+file = File.open(path)
+arr = file.readline.split(' ').map(&:to_i)
+
+puts "#{arr}"
+
+case meth 
+	when 'min' 
+		find_min(arr)
+	when 'ffp' 
+		find_first_pos(arr)
+	else puts '!!!'
+end
