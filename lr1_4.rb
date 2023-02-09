0f4379939c47213cb914911ea8ae4133e7027f5e
bushmrz <56377675+bushmrz@users.noreply.github.com>
2023-02-09 20:43:18 +0400

    Update lr1_4.rb

--- lr1_4.rb
+++ lr1_4.rb
@@ -1,10 +1,18 @@
-def fun1(arr,r)
+# 11 23 59
+def find_unique(arr) 
+  arr = arr.sort
+  if arr[0]!=arr[1] then return arr[0] else return arr[-1]
+  end 
+end
+
+# 35
+def nearest_el(arr,r)
 	arr1 = arr.map{|x| (x-r).abs }
 	ind = arr1.index(arr1.min)
 	puts "#{arr[ind]}"
-	#puts "#{arr1}"
 end
 
+# 47
 def find_del(x)
 	arr = Array.new
  	x.downto(1){ |del|
@@ -13,19 +21,11 @@ def find_del(x)
  	return arr
 end
 
-def fun2(arr)
+def pos_list(arr)
 	arr2 = arr.map{|x| find_del(x) }
 	arr2 = arr2.flatten.uniq
 	puts "#{arr2}"
 end
 
 
-def fun3(arr)
-	arr_new = arr.map{|x| } 
-end
-arr = [1, 0.5, 0.002, 0.08, 0.02, 0.3, 1.3]
-r = 0.2001
-arr1 = [4,6,8, 14, 18, 15, 20, 25]
 
-#fun1(arr,r)
-fun2(arr1)
