9e8a4e5dbc6cc5ed6b3f25761473d623b4daaf01
bushmrzzz <buuushmrz@mail.ru>
2023-03-17 18:05:44 +0300

    lab 2 task 1_2

--- lab2/Student.rb
+++ lab2/Student.rb
@@ -1,15 +1,15 @@
 class Student
 
   # конструктор
-  def initialize(last_name, first_name, second_name, options = {})
-    self.last_name = last_name
-    self.first_name = first_name
-    self.second_name = second_name
-    self.id = [options:id]
-    self.phone = [options:phone]
-    self.mail = [options:mail]
-    self.telegram = [options:telegram]
-    self.git = [options:git]
+  def initialize(last_name, first_name, second_name, id=nil, phone=nil, mail=nil, telegram=nil, git=nil )
+    @last_name = last_name
+    @first_name = first_name
+    @second_name = second_name
+    @id = id
+    @phone = phone
+    @mail = mail
+    @telegram = telegram
+    @git = git
   end
 
   def get_lastname
@@ -76,4 +76,26 @@ class Student
     @git = new_git
   end
 
+  def to_s
+    if @id != nil
+      id_info = "ID: #{@id}"
+    end
+
+    fio_info = "ФИО: #{@last_name} #{@first_name} #{@second_name}"
+
+    if have_any_contact
+      contacts_info = "Контакты:"
+
+      contacts_info += @phone == nil ? "" : "\n\tТелефон: #{@phone}"
+      contacts_info += @telegram == nil ? "" : "\n\tТелеграм: #{@telegram}"
+      contacts_info += @mail == nil ? "" : "\n\tПочта: #{@mail}"
+      contacts_info += @git == nil ? "" :  "\n\tГит: #{@git}"
+    end
+
+    [id_info, fio_info, contacts_info].join("\n")
+  end
+
+  def have_any_contact
+    @phone != nil || @telegram  != nil || @mail != nil || @git != nil
+  end
 end
---
+++ lab2/main.rb
@@ -0,0 +1,9 @@
+require_relative './Student.rb'
+
+student1 = Student.new("Шнайдер", "Ия", "Александровна", "9898", nil, "sixzerosixbae@vk.com", "@darlinmrz", nil)
+student2 = Student.new("Киц", "Олег", "Валерьевич")
+student3 = Student.new("Иванов", "Иван", "Иванович", "2", "999898989")
+
+puts student1
+puts student2
+puts student3
