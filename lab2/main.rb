c33f44031e6f9bb4179eba62a63f3a29ac54d05a
bushmrzzz <buuushmrz@mail.ru>
2023-03-18 21:36:49 +0300

    lab 2 task 2_4

--- lab2/main.rb
+++ lab2/main.rb
@@ -1,12 +1,19 @@
 require_relative './Student.rb'
-
+require_relative './student_short.rb'
 # student1 = Student.new(last_name: "Шнайдер", first_name: "Ия", second_name: "Александровна", opt: {id: "9898", mail: "sixzerosixbae@vk.com", telegram: "@darlinmrz", git: "@bushokmrz"})
-# student2 = Student.new(last_name: "Киц", first_name: "Олег", second_name: "Валерьевич", opt: {phone: "+79009090900"})
+student2 = Student.new(last_name: "Киц", first_name: "Олег", second_name: "Валерьевич", opt: {id: "99", phone: "+79009090900"})
 # student3 = Student.new(last_name: "Иванов", first_name: "Иван", second_name: "Иванович", opt: {id: "2", phone: "+7 999-898-98-89"})
 #
 # puts student1
 # puts student2
 # puts student3
 
-student4 = Student.parse_str("last_name: Test; first_name: Test; second_name: Test; git: @bulba; telegram: @bulba228")
-puts student4.getInfo
+student4 = Student.parse_str("last_name: Test; first_name: Test; second_name: Test; git: @bulba; mail: bulba@bulba.com")
+puts student4
+# puts student4.getInfo
+
+# student5 = Student_short.from_student(student4)
+# puts student5
+
+student6 = Student_short.parse_str(12, "fio: Шнайдер И.А.; git: @darling; mail: hater@hate.ru")
+puts student6
--- lab2/student_short.rb
+++ lab2/student_short.rb
@@ -1,4 +1,44 @@
 # frozen_string_literal: true
-class Student_short
-  # ya gonna sleep sorry
+require_relative './Student.rb'
+class Student_short < Student
+  attr_reader :id, :fio, :git, :mail
+
+  def initialize(id:, fio:, git:, mail:)
+    @id = id
+    @fio = fio
+    self.git = git
+    self.mail = mail
+  end
+
+  def self.from_student(student)
+    Student_short.new(
+      id: student.id,
+      fio: "#{student.last_name}.#{student.first_name.upcase[0]}.#{student.second_name.upcase[0]}.",
+      git: student.git,
+      mail: student.mail
+    )
+  end
+
+  def self.parse_str(id, string)
+    parse_info = string.tr(" ",'').split(";").
+      map{ |x|  x1, x2  = x.split(":")
+      [x1.to_sym, x2] }.to_h
+
+    Student_short.new(
+      id: id,
+      fio: parse_info[:fio],
+      git: parse_info[:git],
+      mail: parse_info[:mail]
+    )
+  end
+  def to_s
+    if id != nil
+      id_info = "ID: #{id}"
+      else id_info = ""
+    end
+
+    fio_info = "ФИО: #{fio}"
+
+    [id_info, fio_info,"Гит: #{git}", "Почта: #{mail}"].join("\n")
+  end
 end
