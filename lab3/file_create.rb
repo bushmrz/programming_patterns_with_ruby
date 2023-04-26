5d30367282062b458472811d7d04ac6a4912ae38
bushmrzzz <buuushmrz@mail.ru>
2023-04-26 13:29:31 +0300

    lr 3 ex refactor

--- lab3/file_create.rb
+++ lab3/file_create.rb
@@ -1,6 +1,9 @@
-CREATE DATABASE studentdb DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+require 'sqlite3'
 
-create table student(
+db = SQLite3::Database.new 'students_db.db'
+
+db.execute <<-SQL
+    create table student(
                         id integer unique not null primary key,
                         last_name varchar(50) not null,
                         first_name varchar(50) not null,
@@ -10,3 +13,6 @@ create table student(
                         telegram varchar(50),
                         mail varchar(50)
 );
+SQL
+
+
